c610ea6f25c85472de64b5c1cd6d68f4fdae4939
Mr-Filson <volkovaelena19730415@gmail.com>
2024-11-10 17:06:07 +0800

    Add files via upload
    gg

---
+++ javahelp.js
@@ -0,0 +1,8 @@
+document.getElementById('theme').addEventListener('click', function() {
+    const currentTheme = document.body.className;
+    if (currentTheme === 'light-theme') {
+        document.body.className = 'dark-theme';
+    } else {
+        document.body.className = 'light-theme';
+    }
+});
